f634088a2bd6a7af8ef25f79a0fbe326df88ea57
Ngoc Qui <quytranlx123@gmail.com>
2025-04-07 06:26:31 +0700

    Login

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/main/resources/templates/**/*.html",   // Đối với các file Thymeleaf
+    "./src/main/resources/static/js/**/*.js",     // Đối với các file JS nếu có
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
