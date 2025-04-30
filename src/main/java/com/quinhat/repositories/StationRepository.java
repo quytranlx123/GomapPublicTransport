ff1c86042608fda2cf5e0b4ac149bdac2f9379c9
Ngoc Qui <125120853+quytranlx123@users.noreply.github.com>
2025-05-01 01:52:09 +0700

    oke

--- src/main/java/com/quinhat/repositories/StationRepository.java
+++ src/main/java/com/quinhat/repositories/StationRepository.java
@@ -1,12 +1,23 @@
 package com.quinhat.repositories;
 
+import com.quinhat.pojo.RouteStation;
 import com.quinhat.pojo.Station;
 import java.util.List;
+import java.util.Map;
 
 public interface StationRepository {
 
+    //QUi
     List<Station> getAllStations();
 
     void save(Station station);
+    //Qui
+    
+    
+    List<Station> findNearestStations(float latitude, float longitude, int limit);
+
+    List<Station> getStations(Map<String, String> params);
+
+    List<RouteStation> getStationsByRouteId(int routeId);
 
 }
--- src/main/java/com/quinhat/repositories/impl/StationRepositoryImpl.java
+++ src/main/java/com/quinhat/repositories/impl/StationRepositoryImpl.java
@@ -1,9 +1,17 @@
 package com.quinhat.repositories.impl;
 
+import com.quinhat.pojo.RouteStation;
 import com.quinhat.pojo.Station;
 import com.quinhat.repositories.StationRepository;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.stereotype.Repository;
@@ -13,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class StationRepositoryImpl implements StationRepository {
 
+    private static final int PAGE_SIZE = 6;
     @Autowired
     private LocalSessionFactoryBean factory;
 
@@ -29,7 +38,82 @@ public class StationRepositoryImpl implements StationRepository {
             s.persist(station);
         } else {
             s.merge(station);
-          
+
         }
     }
+
+    @Override
+    public List<Station> findNearestStations(float latitude, float longitude, int limit) {
+        Session s = this.factory.getObject().getCurrentSession();
+
+        String hql = "SELECT st FROM Station st "
+                + "ORDER BY (POWER(st.latitude - :latitude, 2) + POWER(st.longitude - :longitude, 2)) ASC";
+
+        Query<Station> query = s.createQuery(hql, Station.class);
+        query.setParameter("latitude", latitude);
+        query.setParameter("longitude", longitude);
+        query.setMaxResults(limit);
+
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Station> getStations(Map<String, String> params) {
+        Session s = this.factory.getObject().getCurrentSession();
+
+        CriteriaBuilder b = s.getCriteriaBuilder();
+        CriteriaQuery<Station> q = b.createQuery(Station.class);
+        Root root = q.from(Station.class);
+        q.select(root);
+
+        if (params != null) {
+            List<Predicate> predicates = new ArrayList<>();
+
+            String kw = params.get("kw");
+            if (kw != null && !kw.isEmpty()) {
+                predicates.add(b.like(root.get("name"), String.format("%%%s%%", kw)));
+            }
+
+            String routeId = params.get("routeId");
+            if (routeId != null && !routeId.isEmpty()) {
+                predicates.add(b.equal(root.get("routeId").as(Integer.class), routeId));
+            }
+
+            q.where(predicates.toArray(Predicate[]::new));
+
+            String orderBy = params.get("orderBy");
+            if (orderBy != null && !orderBy.isEmpty()) {
+                q.orderBy(b.asc(root.get(orderBy)));
+            }
+        }
+
+        Query query = s.createQuery(q);
+
+        if (params != null && params.containsKey("page")) {
+            int page = Integer.parseInt(params.get("page"));
+            int start = (page - 1) * PAGE_SIZE;
+
+            query.setMaxResults(PAGE_SIZE);
+            query.setFirstResult(start);
+        }
+
+        return query.getResultList();
+    }
+
+    @Override
+    public List<RouteStation> getStationsByRouteId(int routeId) {
+        Session s = this.factory.getObject().getCurrentSession();
+
+        // Truy vấn RouteStation, JOIN FETCH để lấy luôn Station tương ứng
+        String hql = "SELECT rs FROM RouteStation rs "
+                + "JOIN FETCH rs.stationId "
+                + "WHERE rs.routeId.id = :routeId "
+                + "ORDER BY rs.orderStation ASC";
+
+        Query<RouteStation> query = s.createQuery(hql, RouteStation.class);
+        query.setParameter("routeId", routeId);
+
+        return query.getResultList();
+    }
+
 }
