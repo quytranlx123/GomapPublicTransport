41b2e0da44bb1b6c5ec69a7ace3139912da2df91
Ngoc Qui <125120853+quytranlx123@users.noreply.github.com>
2025-05-12 18:39:11 +0700

    Update SecurityWebApplicationInitializer.java

--- src/main/java/com/quinhat/configs/SecurityWebApplicationInitializer.java
+++ src/main/java/com/quinhat/configs/SecurityWebApplicationInitializer.java
@@ -4,12 +4,61 @@
  */
 package com.quinhat.configs;
 
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  *
  * @author admin
  */
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-    
+@Configuration
+@EnableWebMvc
+@EnableTransactionManagement
+@ComponentScan(basePackages = {
+    "com.quinhat.controllers",
+    "com.quinhat.repositories",
+    "com.quinhat.services"
+})
+public class WebAppContextConfigs implements WebMvcConfigurer {
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+//    @Override
+//    public void addFormatters(FormatterRegistry registry) {
+//        registry.addFormatter(new CategoryFormatter());
+//    }
+    @Bean
+    public StandardServletMultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
 }
