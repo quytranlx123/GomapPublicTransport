efc0d8c982a258f986a4887c1b54210e1b82b15e
Ngoc Qui <125120853+quytranlx123@users.noreply.github.com>
2025-05-12 18:33:52 +0700

    Update ApiTrafficReportController.java

--- src/main/java/com/quinhat/controllers/ApiTrafficReportController.java
+++ src/main/java/com/quinhat/controllers/ApiTrafficReportController.java
@@ -5,33 +5,25 @@
 package com.quinhat.controllers;
 
 import com.quinhat.dto.ApiResponse;
-import com.quinhat.dto.Resquest.TrafficReportRequest;
 import com.quinhat.dto.TrafficReportDTO;
-import com.quinhat.dto.UserDTO;
-import com.quinhat.mapper.TrafficReportMapper;
 import com.quinhat.pojo.TrafficReport;
 import com.quinhat.pojo.User;
 import com.quinhat.services.TrafficReportService;
 import com.quinhat.services.UserService;
-import com.quinhat.utils.JwtUtils;
-import jakarta.ws.rs.core.Response;
 import java.security.Principal;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -47,20 +39,8 @@ public class ApiTrafficReportController {
     private TrafficReportService trafficReportService;
 
     @Autowired
-    private UserService UserService;
-
-    @Autowired
     private UserService userDetailsService;
 
-//    @GetMapping("/traffic-reports")
-//    public ResponseEntity<List<TrafficReportDTO>> getTrafficReports(@RequestParam Map<String, String> params) {
-//        List<TrafficReport> reports = trafficReportService.getTrafficReports(params);
-//        List<TrafficReportDTO> dtos = reports.stream()
-//                .map(TrafficReportMapper::toDTO)
-//                .toList();
-//
-//        return ResponseEntity.ok(dtos);
-//    }
     @GetMapping("/traffic-reports")
     public ResponseEntity<ApiResponse<List<TrafficReportDTO>>> getTrafficReports(Principal principal) {
         String username = principal.getName();
@@ -71,7 +51,7 @@ public class ApiTrafficReportController {
 
         List<TrafficReport> reports = trafficReportService.getTrafficReports(params);
         List<TrafficReportDTO> dtos = reports.stream()
-                .map(TrafficReportMapper::toDTO)
+                .map(TrafficReportDTO::fromEntity)
                 .toList();
 
         ApiResponse<List<TrafficReportDTO>> response = new ApiResponse<>(dtos, HttpStatus.OK.value());
